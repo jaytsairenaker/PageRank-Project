c708325b29d335fa369e5ed84ad932167e8d56d8
Jay Renaker <jayrenaker@MacBook-Air-498.local>
2024-10-02 19:43:03 -0700

    done

--- pagerank.py
+++ pagerank.py
@@ -9,9 +9,9 @@ import math
 import torch
 import gzip
 import csv
+import numpy
 
 import logging
-
 
 class WebGraph():
 
@@ -67,10 +67,15 @@ class WebGraph():
                 last_i = i
 
         # generate the sparse matrix
+        # a tensor is a multidimensional array (like a NumPy array)
         i = torch.LongTensor(indices).t()
+            # contains long integers
         v = torch.FloatTensor(values)
+            # can do math operations w/ values
         n = len(self.url_dict)
         self.P = torch.sparse.FloatTensor(i, v, torch.Size([n,n]))
+            # putting together the values and the indices of the matrix
+
         self.index_dict = {v: k for k, v in self.url_dict.items()}
     
 
@@ -98,14 +103,21 @@ class WebGraph():
         all other entries are set to 0.
         '''
         n = self.P.shape[0]
-
         if query is None:
             v = torch.ones(n)
-
         else:
             v = torch.zeros(n)
             # FIXME: implement Task 2
-        
+            # for each index in the personalization vector:
+
+            for index in range(n):
+                # get the url for the index 
+                url = self._index_to_url(index)
+                # check if the url satisfies the input query
+                if url_satisfies_query(url, query):
+                    # set the corresponding index to one
+                    v[index]=1            
+
         v_sum = torch.sum(v)
         assert(v_sum>0)
         v /= v_sum
@@ -116,47 +128,60 @@ class WebGraph():
     def power_method(self, v=None, x0=None, alpha=0.85, max_iterations=1000, epsilon=1e-6):
         '''
         This function implements the power method for computing the pagerank.
-
         The self.P variable stores the $P$ matrix.
         You will have to compute the $a$ vector and implement Equation 5.1 from "Deeper Inside Pagerank."
         '''
         with torch.no_grad():
             n = self.P.shape[0]
-
+            # grabs the dimensions of the nxn matrix P
             # create variables if none given
             if v is None:
                 v = torch.Tensor([1/n]*n)
                 v = torch.unsqueeze(v,1)
+                # adds a new dimension of size 1 to a tensor at a specified position
             v /= torch.norm(v)
+            # applies the L2 norm to the vector v
 
             if x0 is None:
                 x0 = torch.Tensor([1/(math.sqrt(n))]*n)
                 x0 = torch.unsqueeze(x0,1)
+                # adding another dimension, so this can go in sparse.addmm
+                # 1xnx1??
             x0 /= torch.norm(x0)
-
-            # main loop
-            xprev = x0
-            x = xprev.detach().clone()
-            for i in range(max_iterations):
-                xprev = x.detach().clone()
-
-                # compute the new x vector using Eq (5.1)
+            # normalizing x0
+        
+            # compute the new x vector using Eq (5.1)
                 # FIXME: Task 1
                 # HINT: this can be done with a single call to the `torch.sparse.addmm` function,
                 # but you'll have to read the code above to figure out what variables should get passed to that function
                 # and what pre/post processing needs to be done to them
+            # main loop
+            xprev = x0
+            x = xprev.detach().clone()
+            # creating the new vector AKA x^k
+            
+            # creating the vector a, whose elements are a 1 if dangling node, else 0 
+            stochastic_rows = torch.sparse.sum(self.P, 1).indices()
+            a = torch.ones([n,1])
+            a[stochastic_rows] = 0
+            
+            for i in range(max_iterations):
+                xprev = x.detach().clone()
+                # calculating the second half of equation 5.1
+                equation_tail = (alpha*x.t() @ a + (1-alpha))*v.t()
+                # matrix multiplication and addition
+                x = self.P.t().matmul(x).mul_(alpha).add_(equation_tail.t())
+                # normalizing the vector
+                x /= torch.norm(x)
 
                 # output debug information
                 residual = torch.norm(x-xprev)
                 logging.debug(f'i={i} residual={residual}')
-
                 # early stop when sufficient accuracy reached
                 if residual < epsilon:
                     break
-
             #x = x0.squeeze()
             return x.squeeze()
-
 
     def search(self, pi, query='', max_results=10):
         '''
@@ -245,3 +270,4 @@ if __name__=='__main__':
     v = g.make_personalization_vector(args.personalization_vector_query)
     pi = g.power_method(v, alpha=args.alpha, max_iterations=args.max_iterations, epsilon=args.epsilon)
     g.search(pi, query=args.search_query, max_results=args.max_results)
+ 
